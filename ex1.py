2cb26e6b26705e7cb2253917bad4c86dae9427b2
davidgower1 <34871965+davidgower1@users.noreply.github.com>
2018-01-25 20:12:21 -0700

    Test

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hellow World!")
+print("Hellow Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("This is so cool!")
